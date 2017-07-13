2daf28033f21ec34fc4f10b405c8059e562e7790
Dimitri Sokolyuk <demon@dim13.org>
2017-07-13 15:10:13 +0200

    Split

---
+++ latex.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func execute(w io.Writer, dir string) error {
+	out := new(bytes.Buffer)
+	cmd := exec.Command("latexmk", "-pdf", "-interaction=nonstopmode", "-jobname=out")
+	cmd.Dir = dir
+	cmd.Stdout = out
+	cmd.Stderr = out
+	if err := cmd.Run(); err != nil {
+		return errors.New(out.String() + err.Error())
+	}
+
+	// copy out
+	fd, err := os.Open(filepath.Join(dir, "out.pdf"))
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	_, err = io.Copy(w, fd)
+	return err
+}
+
+func savePart(r *multipart.Reader, dir string) error {
+	part, err := r.NextPart()
+	if err != nil {
+		return err
+	}
+	defer part.Close()
+
+	fname := part.FileName()
+	if fname == "" {
+		return errors.New("no filename specified")
+	}
+	fd, err := os.Create(filepath.Join(dir, filepath.Base(fname)))
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	_, err = io.Copy(fd, part)
+	return err
+}
+
+func saveAllParts(r *multipart.Reader, dir string) error {
+	for {
+		err := savePart(r, dir)
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func Print(w io.Writer, r *multipart.Reader) error {
+	// create temporary working directory
+	dir, err := ioutil.TempDir("", "latex")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(dir)
+
+	if err := saveAllParts(r, dir); err != nil {
+		return err
+	}
+
+	return execute(w, dir)
+}
--- main.go
+++ main.go
@@ -1,81 +1,26 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"io"
-	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"time"
 )
-
-func recvFiles(dir string, r *multipart.Reader) error {
-	part, err := r.NextPart()
-	if err != nil {
-		return err
-	}
-	defer part.Close()
-
-	fname := part.FileName()
-	if fname == "" {
-		return nil
-	}
-
-	log.Println("recv", fname)
-	fd, err := os.Create(filepath.Join(dir, filepath.Base(fname)))
-	if err != nil {
-		return err
-	}
-	defer fd.Close()
-
-	_, err = io.Copy(fd, part)
-	return err
-}
 
 func texHandler(w http.ResponseWriter, r *http.Request) error {
 	defer func(t time.Time) {
 		log.Println("execution time", time.Since(t))
 	}(time.Now())
 
-	dir, err := ioutil.TempDir("", "gotex")
-	if err != nil {
-		return err
-	}
-	defer os.RemoveAll(dir)
-
 	mr, err := r.MultipartReader()
 	if err != nil {
 		return err
 	}
-	for {
-		err := recvFiles(dir, mr)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-	}
-
-	out := new(bytes.Buffer)
-	cmd := exec.Command("latexmk", "-pdf", "-interaction=nonstopmode", "-jobname=out")
-	cmd.Dir = dir
-	cmd.Stdout = out
-	cmd.Stderr = out
-
-	if err := cmd.Run(); err != nil {
-		return errors.New(out.String() + err.Error())
-	}
-
-	return sendFile(filepath.Join(dir, "out.pdf"), w, r)
+	return Print(w, mr)
 }
 
-func sendFile(fname string, w http.ResponseWriter, r *http.Request) error {
+func indexHandler(fname string, w http.ResponseWriter, r *http.Request) error {
 	fd, err := os.Open(fname)
 	if err != nil {
 		return err
@@ -89,7 +34,7 @@ func sendFile(fname string, w http.ResponseWriter, r *http.Request) error {
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		if err := sendFile("index.html", w, r); err != nil {
+		if err := indexHandler("index.html", w, r); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	case http.MethodPost:
